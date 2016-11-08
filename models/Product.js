c4350fa135ccdfe1edd32ba7c72067b13792d36d
Marco Antonio Chagolla <yurakawada@gmail.com>
2016-11-08 10:04:16 -0600

    Adding models and mongoose connection

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ 
+ var productSchema = new Schema({
+   name: String,
+   photo: String,
+   stock: Number
+ });
+module.export = mongoose.model('Product', productSchema);
